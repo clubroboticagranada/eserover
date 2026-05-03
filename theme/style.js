408452c5f7f4b758164b9801aa6b35ea96562ba0
pedroruizf <pedroruizf@gmail.com>
2026-05-03 17:24:31 +0200

    cambios en programas para cutebot e iot

--- theme/style.js
+++ theme/style.js
@@ -43,7 +43,6 @@ var myTheme = {
                         myTheme.params('add');
                     }
                 }
-                window.scroll(0, 0);
             } else {
                 $('body').toggleClass('siteNav-off');
                 myTheme.params(
@@ -62,19 +61,19 @@ var myTheme = {
                 }
                 bar.show();
                 $('#exe-client-search-text').focus();
-                window.scroll(0, 0);
             }
         });
-        if (!this.inIframe()) {
-            // Fixed navigation
-            $('#siteNav').wrap('<div id="sidebar-nav"></div>');
+        // Fixed navigation
+        $('#siteNav').wrap('<div id="sidebar-nav"></div>');
+        myTheme.checkNav();
+        $(window).bind('resize', function () {
             myTheme.checkNav();
-            $(window).bind('resize', function () {
-                myTheme.checkNav();
-            });
-        }
+        });
         // Search form
         this.searchForm();
+
+        // mover .page-title dentro de .page-content
+        this.movePageTitle();
     },
     inIframe: function () {
         try {
@@ -118,10 +117,43 @@ var myTheme = {
             myTheme.param(this, act);
         });
     },
+
+    // function that move the h2 outside the header
+    movePageTitle: function () {
+        const tryMove = () => {
+            const $header = $('.main-header .page-header');
+            const $title = $header.find('.page-title').first();
+
+            // Search container of content
+            let $content = $('.page-content').first();
+            if (!$content.length)
+                $content = $('.content, main .content').first();
+            if (!$content.length) $content = $('#main, #content').first();
+            if (!$content.length && $header.length)
+                $content = $header.nextAll(':not(header)').first();
+            if (!$content.length && $header.length) $content = $header.parent();
+
+            if ($header.length && $title.length && $content.length) {
+                $content.prepend($title); // move it to the start
+                return true;
+            }
+            return false;
+        };
+
+        if (tryMove()) return;
+
+        const observer = new MutationObserver(() => {
+            if (tryMove()) observer.disconnect();
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    },
+    // 🔼
 };
+
 $(function () {
     myTheme.init();
 });
+
 $.fn.isInViewport = function () {
     var elementTop = $(this).offset().top;
     var elementBottom = elementTop + $(this).outerHeight();
